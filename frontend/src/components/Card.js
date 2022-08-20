987ef23ec5259f4c1e17e87dc4ec9a62b8ce06af
unknown <vitaminka_94@mail.ru>
2022-08-20 16:24:46 +0300

    fix

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -17,14 +17,14 @@ const Card = ({ card, onCardClick, onCardLike, onCardDelete }) => {
 
   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
 
-  // const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = card.likes.some((i) => i._id === currentUser._id);
 
-  let isLiked;
-  if (typeof card.likes !== "undefined") {
-    isLiked = card.likes.some((i) => i._id === currentUser._id);
-  } else {
-    isLiked = false;
-  } 
+  // let isLiked;
+  // if (typeof card.likes !== "undefined") {
+  //   isLiked = card.likes.some((i) => i._id === currentUser._id);
+  // } else {
+  //   isLiked = false;
+  // } 
   
   // Создаём переменную, которую после зададим в `className` для кнопки лайка
   const cardLikeButtonClassName = `element__like ${
